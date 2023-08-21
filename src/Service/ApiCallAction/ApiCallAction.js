77aa46673cc609dd028e19d4cbb493aff62d29ee
Adarsh9991 <93855165+Adarsh9991@users.noreply.github.com>
2023-08-21 21:31:14 +0530

    Update ApiCallAction.js

--- src/Service/ApiCallAction/ApiCallAction.js
+++ src/Service/ApiCallAction/ApiCallAction.js
@@ -2,10 +2,10 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const ApiCallAction = async (method, endpoint, data = null) => {
-  const token = process.env.REACT_APP_API_TOKEN
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWU2ZWVjNjIwYjE3ZmQ0YWI1NDA0Nzg5NWE2MGNhZSIsInN1YiI6IjY0ZTAzNDg1YTNiNWU2MDFkYjFhYmFkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QD-9wWbDbJ-NOsB17UDIimNckNhV-lqTfYz74370Oa0"
     
 
   const config = {
